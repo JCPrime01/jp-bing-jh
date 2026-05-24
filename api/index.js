fbe9668d51959dabfce8c0295e7e3046e1bfa174
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-24 19:34:37 -0300

    chore: update shareCode to U19R4HW2UZT

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=32YGBAJCV56&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=U19R4HW2UZT&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
