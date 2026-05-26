b759c5873b4ef80613c777a0fdc54232784b2df4
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-26 19:23:02 -0300

    chore: update shareCode to 1D1VIFEQE7A

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=USJUXZFK8PU&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=1D1VIFEQE7A&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
